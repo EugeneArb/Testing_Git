c586a41b56dffe0d7f135ff1b7ff434462648f7a
EugeneArb <evgeshagarbuz@gmail.com>
2025-03-22 13:30:36 +0200

    Додавання тесту "Сортування контактів за іменем та телефоном"

--- contacts/cypress/e2e/ContactsTests.cy.js
+++ contacts/cypress/e2e/ContactsTests.cy.js
@@ -36,4 +36,36 @@ describe('Contacts-App Tests', () => {
   
     cy.get('.contacts-list li').should('have.length', 0);
   });
+
+  it('Test for sorting contacts.', () => {
+    cy.get('input[placeholder="Ім\'я"]').type('Voronaia Kira');
+    cy.get('input[placeholder="Телефон"]').type('33333333333');
+    cy.get('button').contains('Додати').click();
+  
+    cy.get('input[placeholder="Ім\'я"]').type('Saniko Karina');
+    cy.get('input[placeholder="Телефон"]').type('11111111111');
+    cy.get('button').contains('Додати').click();
+  
+    cy.get('input[placeholder="Ім\'я"]').type('Garbuz Eugene');
+    cy.get('input[placeholder="Телефон"]').type('222222222222');
+    cy.get('button').contains('Додати').click();
+
+    cy.get('input[placeholder="Ім\'я"]').type('Byzivskui Artem');
+    cy.get('input[placeholder="Телефон"]').type('55555555555');
+    cy.get('button').contains('Додати').click();
+  
+    // Сортуємо за іменем
+    cy.get('button').contains('Сортувати за іменем').click();
+    cy.get('.contacts-list li span').eq(0).should('have.text', 'Byzivskui Artem - 55555555555');
+    cy.get('.contacts-list li span').eq(1).should('have.text', 'Garbuz Eugene - 222222222222');
+    cy.get('.contacts-list li span').eq(2).should('have.text', 'Saniko Karina - 11111111111');
+    cy.get('.contacts-list li span').eq(3).should('have.text', 'Voronaia Kira - 33333333333');
+  
+    // Сортуємо за телефоном
+    cy.get('button').contains('Сортувати за телефоном').click();
+    cy.get('.contacts-list li span').eq(0).should('have.text', 'Saniko Karina - 11111111111');
+    cy.get('.contacts-list li span').eq(1).should('have.text', 'Garbuz Eugene - 222222222222');
+    cy.get('.contacts-list li span').eq(2).should('have.text', 'Voronaia Kira - 33333333333');
+    cy.get('.contacts-list li span').eq(3).should('have.text', 'Byzivskui Artem - 55555555555');
+  });
 });
