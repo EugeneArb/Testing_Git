e95f2d3756228fcb2f0e584e7a344af566c58bca
EugeneArb <evgeshagarbuz@gmail.com>
2025-03-22 12:49:14 +0200

    Додавання тесту "Редагування контакта"

--- contacts/cypress/e2e/ContactsTests.cy.js
+++ contacts/cypress/e2e/ContactsTests.cy.js
@@ -13,4 +13,19 @@ describe('Contacts-App Tests', () => {
     cy.get('.contacts-list li').should('have.length', 1);
     cy.get('.contacts-list li span').first().should('have.text', 'Eugene Garbuz - 28623674267');
   });
+
+  it('Test for editing a contact.', () => {
+    cy.get('input[placeholder="Ім\'я"]').type('Eugene Garbuz');
+    cy.get('input[placeholder="Телефон"]').type('28623674267');
+    cy.get('button').contains('Додати').click();
+  
+    cy.get('.contacts-list li button').contains('Редагувати').click();
+    cy.get('input[placeholder="Ім\'я"]').clear().type('Genia Harbuz');
+    cy.get('input[placeholder="Телефон"]').clear().type('99999999999');
+    cy.get('button').contains('Редагувати').click();
+  
+    cy.get('.contacts-list li span').first().should('have.text', 'Genia Harbuz - 99999999999');
+  });
+
+  
 });
